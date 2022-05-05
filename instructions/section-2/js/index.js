8f0692ad55a6fb5b12e09a640559e76aef19aab5
Jason Edokpa <jasonedokpa@gmail.com>
2022-05-04 19:03:28 -0500

    added: show GitHub repositories fixed some formatting with index.html

--- instructions/section-2/js/index.js
+++ instructions/section-2/js/index.js
@@ -1,4 +1,3 @@
-
 const today = new Date();
 const thisYear = today.getFullYear();
 const footer = document.querySelector('footer');
@@ -10,15 +9,18 @@ footer.appendChild(copyright);
 const skills = ["HTML", "CSS", "Javascript"];
 const skillsSection = document.getElementById('skills');
 const skillsList = skillsSection.querySelector('ul');
-for (i = 0; i < skills.length; i++) {
+for (i = 0; i < skills.length; i++)
+{
     const skill = document.createElement('li');
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 };
+
 //DOM
 const messageForm = document.getElementsByName("leave_message")[0];
 console.log(messageForm);
-messageForm.addEventListener('submit', (event) => {
+messageForm.addEventListener('submit', (event) =>
+{
     event.preventDefault();
     
     const name = event.target.name;
@@ -37,7 +39,8 @@ messageForm.addEventListener('submit', (event) => {
 
     removeButton.type = 'button'
 
-    removeButton.addEventListener('click', (event) => {
+    removeButton.addEventListener('click', (event) =>
+    {
         const entry = event.target.parentNode
         entry.remove()
     });
@@ -52,11 +55,13 @@ messageForm.addEventListener('submit', (event) => {
 window.addEventListener('DOMContentLoaded', setup); 
 
 function setup() {
-    const options = {
+    const options = 
+    {
         rootMargin: '0px 0px -200px 0px'
     }
 
-    const observer = new IntersectionObserver((entries, observer) => {
+    const observer = new IntersectionObserver((entries, observer) =>
+    {
         entries.forEach(entry => {
             if(entry.isIntersecting) {
                 entry.target.classList.add('show');
@@ -76,22 +81,33 @@ function setup() {
 }
 
 //JSON
-
 var githubRequest = new XMLHttpRequest();
 githubRequest.open('GET', 'https://api.github.com/users/Armanchi/repos');
 githubRequest.send();
-githubRequest.addEventListener('load', (event) => {
-    
+githubRequest.addEventListener('load', function (event)
+{
     const repositories = JSON.parse(this.response);
-    console.log(repositories); 
+    console.log("Repositories:", repositories);
 
+    const projectElement = document.getElementById('projects');
+    const projectList = projectElement.querySelector('ul');
+
+    for (i = 0; i < repositories.length; i++ )
+    {
+        const project = document.createElement('li');
+        project.innerText = repositories[i].name;
+        projectList.appendChild(project);
+    };
+
+    //Stetch Goal: Transform your repository names into `<a>` tags that link to GitHub
+    /* for (i = 0; i < repositories.length; i++)
+    {
+        const project = document.createElement('li');
+        const projectLink = document.createElement('a');
+        projectLink.textContent = repositories[i].name;
+        projectLink.setAttribute("href", repositories[i].html_url);
+        project.appendChild(projectLink);
+        projectList.appendChild(project);
+        console.log("List", projectList);
+    } */
 });
-
-const projectSection = document.getElementById('projects');
-const projectList = projectSection.querySelector('ul');
-for (i = 0; i < repositories.length; i++ ){
-    const project = document.createElement('li');
-    project.innerText = project[i];
-    project.appendChild(projectList);
-
-};

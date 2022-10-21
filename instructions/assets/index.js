a9eca80f1dad28e4422bcc46a33c371b46183f66
Armanchi <armanijohnson0104@gmail.com>
2022-10-21 15:06:55 -0500

    updated index.html & index.css

--- instructions/assets/index.js
+++ instructions/assets/index.js
@@ -44,36 +44,36 @@ function setup() {
 
 
 //DOM
-const messageForm = document.getElementsByName("leave_message")[0];
-console.log(messageForm);
-messageForm.addEventListener('submit', (event) => {
-    event.preventDefault();
+// const messageForm = document.getElementsByName("leave_message")[0];
+// console.log(messageForm);
+// messageForm.addEventListener('submit', (event) => {
+//     event.preventDefault();
     
-    const name = event.target.name;
-    const email = event.target.email;
-    const message = event.target.message;
+//     const name = event.target.name;
+//     const email = event.target.email;
+//     const message = event.target.message;
 
-    console.log(name.value, email.value, message.value);
+//     console.log(name.value, email.value, message.value);
 
-    const messageSection = document.getElementById('messages');
-    const messageList = messageSection.querySelector('ul');
-    const newMessage = document.createElement('li');
-    newMessage.innerHTML = `<a href = "mailto:${email.value}" target "_blank"> ${name.value}</a><span> wrote: ${message.value} </span>`
+//     const messageSection = document.getElementById('messages');
+//     const messageList = messageSection.querySelector('ul');
+//     const newMessage = document.createElement('li');
+//     newMessage.innerHTML = `<a href = "mailto:${email.value}" target "_blank"> ${name.value}</a><span> wrote: ${message.value} </span>`
 
-    const removeButton = document.createElement('button');
-    removeButton.innerText = 'remove';
+//     const removeButton = document.createElement('button');
+//     removeButton.innerText = 'remove';
 
-    removeButton.type = 'button'
+//     removeButton.type = 'button'
 
-    removeButton.addEventListener('click', (event) => {
-        const entry = event.target.parentNode
-        entry.remove()
-    });
+//     removeButton.addEventListener('click', (event) => {
+//         const entry = event.target.parentNode
+//         entry.remove()
+//     });
 
-    newMessage.appendChild(removeButton);
-    messageList.appendChild(newMessage);
-    messageForm.reset();  
-});
+//     newMessage.appendChild(removeButton);
+//     messageList.appendChild(newMessage);
+//     messageForm.reset();  
+// });
 
 
 //AJAX
@@ -112,7 +112,7 @@ const projectList = projectSection.querySelector('ul');
 fetch('https://api.github.com/users/Armanchi/repos')
     .then(res => res.json())
     .then(data => {
-            for (let i = 0; i < data.length; i++) {
+            for (let i = 5; i < data.length; i++) {
             const project = document.createElement('li');
             const projectLink = document.createElement('a');
             projectLink.textContent = data[i].name;
